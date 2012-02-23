c5c21eae3a0fe1cc14680eb41ca9442c25959579
intoxstudio <intoxstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-23 08:04:33 +0000

    tagging 0.8.1
    git-svn-id: https://plugins.svn.wordpress.org/content-aware-sidebars/tags/0.8.1@509268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+if(!defined('WP_UNINSTALL_PLUGIN')) {
+	exit();
+}
+
+// Remove db version
+delete_option('cas_db_version');
+
+// Remove all sidebars
+$posts = get_posts(array(
+	'numberposts'	=> -1,
+	'post_type'	=> 'sidebar',
+	'post_status'	=> null
+));
+foreach($posts as $post) {
+	wp_delete_post($post->ID, true);
+}
+
+?>
