36c93dcdf12125921277860b8cf23d0614b9fb46
intoxstudio <intoxstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-05 12:22:41 +0000

    version 2.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/content-aware-sidebars/tags/2.1@908191 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/qtranslate.php
+++ modules/qtranslate.php
@@ -6,42 +6,56 @@
 
 /**
  *
- * bbPress Module
+ * qTranslate Module
  * 
  * Detects if current content is:
- * a) any or specific bbpress user profile
+ * a) in specific language
  *
  */
 class CASModule_qtranslate extends CASModule {
 	
+	/**
+	 * Constructor
+	 */
 	public function __construct() {
-		parent::__construct();
-		$this->id = 'language';
-		$this->name = __('Languages','content-aware-sidebars');
-		
-		add_filter('manage_edit-sidebar_columns',		array(&$this,'admin_column_headers'));
-		
+		parent::__construct('language',__('Languages',ContentAwareSidebars::DOMAIN));
 	}
 	
-	public function is_content() {
+	/**
+	 * Determine if content is relevant
+	 * @return boolean 
+	 */
+	public function in_context() {
 		return true;
 	}
-	
-	public function db_where() {
-		return "(language.meta_value IS NULL OR language.meta_value IN('language','".qtrans_getLanguage()."'))";
-	}
-	
-	public function admin_column_headers($columns) {	
-		unset($columns['language']);	
-		return $columns;
-	}
 
-	public function _get_content() {
+	/**
+	 * Get data from context
+	 * @author Joachim Jensen <jv@intox.dk>
+	 * @since  2.0
+	 * @return array
+	 */
+	public function get_context_data() {
+		return array(
+			$this->id,
+			qtrans_getLanguage()
+		);
+	}
+	
+	/**
+	 * Get languages
+	 * @global array $q_config
+	 * @return array 
+	 */
+	protected function _get_content($args = array()) {
 		global $q_config;
 		$langs = array();
 			
 		foreach(get_option('qtranslate_enabled_languages') as $lng) {
 			$langs[$lng] = $q_config['language_name'][$lng];
+		}
+		if(isset($args['include'])) {
+			$langs = array_intersect_key($langs,array_flip($args['include']));
 		}
 		return $langs;
 	}
